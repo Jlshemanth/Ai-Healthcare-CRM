e6340b5e431a9e23979adaec05d5c0a0dd9155f7
Jlshemanth <jalagadugulahemanth@gmail.com>
2026-05-19 23:48:29 +0530

    Removed API key securely

--- backend/app/agents/hcp_agent.py
+++ backend/app/agents/hcp_agent.py
@@ -1,13 +1,26 @@
 from langchain_groq import ChatGroq
 import os
+from dotenv import load_dotenv
 
-os.environ["GROQ_API_KEY"] = "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"
+# Load environment variables
+
+load_dotenv()
+
+# Get API key from .env
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+# LLM Model
 
 llm = ChatGroq(
-    api_key=os.environ["GROQ_API_KEY"],
+    api_key=GROQ_API_KEY,
     model_name="llama-3.3-70b-versatile"
 )
 
+# AI Agent Function
+
 def ask_agent(message):
+
     response = llm.invoke(message)
+
     return response.content
